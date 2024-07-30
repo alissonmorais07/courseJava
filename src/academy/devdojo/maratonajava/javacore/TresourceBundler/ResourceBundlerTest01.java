80903f35db8e1a00023d1bbfe4523d2bad7c2010
Alisson <alissonmorais177@gmail.com>
2024-07-30 15:54:06 -0300

    ResourceBundler concepts

--- src/academy/devdojo/maratonajava/javacore/TresourceBundler/ResourceBundlerTest01.java
+++ src/academy/devdojo/maratonajava/javacore/TresourceBundler/ResourceBundlerTest01.java
@@ -1,2 +1,18 @@
-package academy.devdojo.maratonajava.javacore.TresourceBundler;public class ResourceBundlerTest01 {
+package academy.devdojo.maratonajava.javacore.TresourceBundler;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBundlerTest01 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", new Locale("en", "US"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+        System.out.println(bundle.getString("hi"));
+        bundle = ResourceBundle.getBundle("messages", new Locale("pt", "BR"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+        System.out.println(bundle.getString("hi"));
+    }
 }
