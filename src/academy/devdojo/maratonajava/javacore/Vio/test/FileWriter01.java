18b5205da360d351cf5ece85298e00c6960b8803
alisson <alissonmorais177@gmail.com>
2024-08-17 20:29:08 -0300

    FileWriter initial concepts

--- src/academy/devdojo/maratonajava/javacore/Vio/test/FileWriter01.java
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/FileWriter01.java
@@ -1,4 +1,17 @@
 package academy.devdojo.maratonajava.javacore.Vio.test;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file)) {
+            fw.write("Inserting data into the file using the write method\n");
+            fw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
