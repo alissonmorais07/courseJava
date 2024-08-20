c10b263abd39bef720e9b3df189c12973934d31a
alisson <alissonmorais177@gmail.com>
2024-08-19 21:27:20 -0300

    BufferedWriter concepts

--- src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file);
-             BufferedWriter br = new BufferedWriter(fw)) {
-            br.write("Inserting data into the file using the write method");
-            br.newLine();
-            br.flush();
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write("Inserting data into the file using the write method");
+            bw.newLine();
+            bw.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
